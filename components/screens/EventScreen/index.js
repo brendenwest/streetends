09f5996ab53a91f25d04574a02717ebe275255ae
AlexSuth <alexandernsutherland@gmail.com>
2020-10-19 18:39:26 -0700

    Creates some of the icons and also adds event screen placeholder

---
+++ components/screens/EventScreen/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    Button,
+    Text,
+    View,
+  } from 'react-native';
+import Header from '../../GlobalComponents/Header';
+import 'react-native-gesture-handler';
+import Footer from '../../GlobalComponents/Footer';
+
+const EventScreen = ( {navigation} ) => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Header />
+        <Text>Events and Meetings Screen</Text>
+        <Button 
+        title='Go Back Home'
+        onPress={() => navigation.navigate("Home")} 
+        />
+        <Footer />
+      </View>
+    );
+  };
+
+  export default EventScreen;
--- components/screens/HomeScreen/index.js
+++ components/screens/HomeScreen/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, Image, View, Text } from 'react-native';
 import 'react-native-gesture-handler';
 import Header from '../../GlobalComponents/Header';
+
+import { Icon } from 'react-native-elements'
 
 const HomeScreen = ( {navigation } ) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Header />
-      <Text>Home Screen</Text>
+      <Text>Shoreline Access for Everyone</Text>
+      <Image 
+        source={{uri: "https://i.imgur.com/F3aMEfG.jpg"}} 
+        style=
+          {{padding: 40, 
+            width: 420, 
+            height: 200, 
+            alignSelf: 'center'}} 
+      />
+        
+      <Icon
+          name='map'
+          size={150}
+          onPress={() => navigation.navigate('Map')}
+      />      
+      
+      <Icon
+          name='event'
+          size={150}
+          onPress={() => navigation.navigate('Map')}
+      />      
+   
       <Button
         title="Go to Map"
         onPress={() => navigation.navigate('Map')}
@@ -21,6 +44,8 @@ const HomeScreen = ( {navigation } ) => {
         onPress={() => navigation.navigate("Let's Get in Touch")}
       />
     </View>
+
+    
   );
 }
 
