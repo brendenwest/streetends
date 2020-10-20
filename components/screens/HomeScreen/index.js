63005cefe2470fd42ff7e1f23a839bb7b54d6df8
AlexSuth <alexandernsutherland@gmail.com>
2020-10-19 18:48:53 -0700

    Adds remaining icons to home screen. All icons work as navigation buttons. Still need to be styled :q

--- components/screens/HomeScreen/index.js
+++ components/screens/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Image, View, Text } from 'react-native';
+import { Button, Image, ScrollView, View, Text } from 'react-native';
 import 'react-native-gesture-handler';
 import Header from '../../GlobalComponents/Header';
 
@@ -7,43 +7,45 @@ import { Icon } from 'react-native-elements'
 
 const HomeScreen = ( {navigation } ) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Header />
-      <Text>Shoreline Access for Everyone</Text>
-      <Image 
-        source={{uri: "https://i.imgur.com/F3aMEfG.jpg"}} 
-        style=
-          {{padding: 40, 
-            width: 420, 
-            height: 200, 
-            alignSelf: 'center'}} 
-      />
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Header />
+        <Text>Shoreline Access for Everyone</Text>
+        <Image 
+          source={{uri: "https://i.imgur.com/F3aMEfG.jpg"}} 
+          style=
+            {{padding: 40, 
+              width: 420, 
+              height: 200, 
+              alignSelf: 'center'}} 
+        />
+          
+        <Icon
+            name='map'
+            size={150}
+            onPress={() => navigation.navigate('Map')}
+        />      
         
-      <Icon
-          name='map'
-          size={150}
-          onPress={() => navigation.navigate('Map')}
-      />      
-      
-      <Icon
-          name='event'
-          size={150}
-          onPress={() => navigation.navigate('Map')}
-      />      
+        <Icon
+            name='event'
+            size={150}
+            onPress={() => navigation.navigate('Events')}
+        />    
+        
+        <Icon
+            name='email'
+            size={150}
+            onPress={() => navigation.navigate("Let's Get in Touch")}
+        />            
+
+        <Icon
+            name='info'
+            size={150}
+            onPress={() => navigation.navigate('About')}
+        />                    
    
-      <Button
-        title="Go to Map"
-        onPress={() => navigation.navigate('Map')}
-      />
-      <Button
-        title="Go to About"
-        onPress={() => navigation.navigate('About')}
-      />
-      <Button
-        title="Go to Contact"
-        onPress={() => navigation.navigate("Let's Get in Touch")}
-      />
-    </View>
+      </View>
+    </ScrollView>
 
     
   );
