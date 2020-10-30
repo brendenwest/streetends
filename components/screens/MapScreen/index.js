6b8eb5b53940766f1bf9951188a941f148afe5b8
David Prismantas <davidprismantas@gmail.com>
2020-10-29 20:09:12 -0700

    added map screen and api key

--- components/screens/MapScreen/index.js
+++ components/screens/MapScreen/index.js
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
+    Platform,
+    Dimensions,
     Button,
+    StyleSheet,
     Text,
     View,
   } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, PROVIDER_DEFAULT } from 'react-native-maps';
 import Header from '../../GlobalComponents/Header';
 import 'react-native-gesture-handler';
 import Footer from '../../GlobalComponents/Footer';
 
+const IOS = Platform.OS === 'ios';
+const ANDROID = Platform.OS === 'android';
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 47.6168776;
+const LONGITUDE = -122.3376114;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const mapStyles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   width: width,
+   height: height,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
+
 const MapScreen = ( {navigation} ) => {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Header />
-        <Text>Map Screen</Text>
-        <Button 
-        title='Go Back Home'
-        onPress={() => navigation.navigate("Home")} 
-        />
-        <Footer />
+      <View style={mapStyles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={mapStyles.map}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+          initialCamera={{
+            center: {
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+            },
+            pitch: 0,
+            heading: 0,
+            altitude: 1000,
+            zoom: 11,
+          }}
+          >
+        </MapView>
       </View>
     );
   };
