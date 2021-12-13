2c3e6effd12cc575075d5d3475d46e693cdc221f
Lola2018 <44072547+Lola2018@users.noreply.github.com>
2021-12-13 05:54:07 -0800

    added contact form and removed secret.xml file

--- components/screens/ContactScreen/contactForm.js
+++ components/screens/ContactScreen/contactForm.js
@@ -8,6 +8,66 @@ import {
 } from 'react-native';
 import {Icon, Button} from 'react-native-elements';
 export default class ContactForm extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      firstName:'', 
+      lastName:'',
+      email:'',
+      message:'',
+    }
+  }
+
+  updateValue(text,field){
+    if(field=='firstName'){
+      this.setState({
+        firstName:text,
+      })
+    }
+    else if(field=='lastName'){
+      this.setState({
+        lastName:text,
+      })
+    }
+    else if(field=='email'){
+      this.setState({
+        email:text,
+       })
+    }
+    else if(field=='message'){
+      this.setState({
+        message:text,
+       })
+    }
+  }
+ 
+  submit(){
+    let collection={}
+    collection.firstName=this.state.firstName;
+    collection.lastName=this.state.lastName;
+    collection.email=this.state.email;
+    collection.message=this.state.message;
+    console.warn(collection);
+  }
+
+  fetchContacts = () => {
+    var url = "https://streetends-api.herokuapp.com/contacts";
+
+    fetch(url, {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(),
+      headers: new Headers({ 'Content-Type': 'application/json'})
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', collection);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+ 
   render() {
     return (
       <View style={styles.container}>
@@ -20,13 +80,14 @@ export default class ContactForm extends Component {
         <Text style={styles.boxtext}>Message</Text>
         <TextInput
           style={styles.input2}
-          ref="body"
+          ref="message"
           placeholder="Enter your message"
           multiline={true}
+          onChangeText={(text)=>this.updateValue(text,'message')}
         />
 
-        <TouchableHighlight>
-          <Button buttonStyle={styles.button} title="Submit" />
+        <TouchableHighlight >
+          <Button buttonStyle={styles.button} title="Submit" onPress={this.fetchContacts}/>
         </TouchableHighlight>
       </View>
     );
